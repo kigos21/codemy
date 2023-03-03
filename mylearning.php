a356bcaa0c4793074013280573ec1d4f3bbc21cb
kigos21 <83705521+kigos21@users.noreply.github.com>
2023-03-03 14:09:36 +0800

    rendering of owned courses added

--- mylearning.php
+++ mylearning.php
@@ -18,7 +18,15 @@
 
 <body>
 
-    <?php include "navbar.html"; ?>
+    <?php
+
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+
+    include "navbar.html";
+
+    ?>
 
     <!-- Main -->
     <div class="main container-md" style="margin-bottom: 20vh;">
@@ -32,6 +40,60 @@
             <div class="col col-md-8">
                 <div class="row row-cols-1 row-cols-xxl-4 row-cols-lg-2 row-cols-md-1 g-4">
                     <!-- TODO: render database content courses -->
+                    <?php
+
+                    // establish a connection to the MySQL database
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "codemy";
+                    $iduser = $_SESSION["iduser"];
+
+                    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+                    // check connection
+                    if (!$conn) {
+                        die("Connection failed: " . mysqli_connect_error());
+                    }
+
+                    // execute the query
+                    $sql = "SELECT idlearning, c.idcourse, imglink, title, description, u.iduser, email
+                    FROM learning l
+                    INNER JOIN user u ON l.iduser = u.iduser
+                    INNER JOIN course c ON l.idcourse = c.idcourse
+                    WHERE u.iduser = '$iduser';";
+                    $result = mysqli_query($conn, $sql);
+
+                    // iterate through the results
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $idcourse = $row["idcourse"];
+                            $src = $row["imglink"];
+                            $title = $row["title"];
+                            $description = $row["description"];
+                            // access the values of each row using the column names
+                            echo "<div class='col'>
+                                <div class='card h-100'>
+                                    <img src='$src' class='card-img-top img-fluid' alt='...'>
+                                    <div class='card-body'>
+                                        <h5 class='card-title'>$title</h5>
+                                        <p class='card-text'>$description</p>
+                                    </div>
+                                    <form action='removefromlearn.php' method='post'>
+                                        <input type='hidden' id='idcourse' name='idcourse' value='$idcourse'>
+                                        <input type='hidden' id='iduser' name='iduser' value='$iduser'>
+                                        <input type='submit' class='removeFromBtn' value='Remove Course'>
+                                    </form>
+                                </div>
+                            </div>";
+                        }
+                    } else {
+                    }
+
+                    // close the connection
+                    mysqli_close($conn);
+
+                    ?>
                 </div>
 
             </div>

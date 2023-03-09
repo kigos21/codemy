4d62009cd211657a6ff42792dc320493ef0d8954
kigos21 <83705521+kigos21@users.noreply.github.com>
2023-03-09 20:46:00 +0800

    added table data in SQL

--- init_db.sql
+++ init_db.sql
@@ -1,22 +1,62 @@
-CREATE SCHEMA IF NOT EXISTS `sample2` ;
+CREATE SCHEMA IF NOT EXISTS `codemy` ;
 
-USE sample2;
+USE codemy;
 
-CREATE TABLE IF NOT EXISTS `sample2`.`course` (
+CREATE TABLE IF NOT EXISTS `codemy`.`course` (
   `idcourse` INT NOT NULL AUTO_INCREMENT,
   `imglink` LONGTEXT NOT NULL,
   `title` VARCHAR(255) NOT NULL,
   `description` LONGTEXT NOT NULL,
   PRIMARY KEY (`idcourse`));
 
-CREATE TABLE IF NOT EXISTS `sample2`.`user` (
+  INSERT INTO `codemy`.`course` (
+    `imglink`, 
+    `title`, 
+    `description`) 
+  VALUES (
+    'https://picsum.photos/seed/1/1600/900', 
+    'The Complete Python Bootcamp', 
+    'Learn Python like a Professional'
+  );
+
+  INSERT INTO `codemy`.`course` (
+    `imglink`, 
+    `title`, 
+    `description`) 
+  VALUES (
+    'https://picsum.photos/seed/2/1600/900', 
+    'Web Developer Bootcamp 2023', 
+    'The only course you need to learn web development'
+  );
+
+  INSERT INTO `codemy`.`course` (
+    `imglink`, 
+    `title`, 
+    `description`) 
+  VALUES (
+    'https://picsum.photos/seed/3/1600/900', 
+    'Java Programming Masterclass', 
+    'Obtain valuable Core Java Skills'
+  );
+
+  INSERT INTO `codemy`.`course` (
+    `imglink`, 
+    `title`, 
+    `description`) 
+  VALUES (
+    'https://picsum.photos/seed/4/1600/900', 
+    'The Complete iOS App Development Bootcamp', 
+    'Master the SwiftUI in just one Course'
+  );
+
+CREATE TABLE IF NOT EXISTS `codemy`.`user` (
   `iduser` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`iduser`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
 
-CREATE TABLE IF NOT EXISTS `sample2`.`learning` (
+CREATE TABLE IF NOT EXISTS `codemy`.`learning` (
   `idlearning` INT NOT NULL AUTO_INCREMENT,
   `iduser` INT NOT NULL,
   `idcourse` INT NOT NULL,
@@ -25,11 +65,11 @@ CREATE TABLE IF NOT EXISTS `sample2`.`learning` (
   INDEX `idcourse_idx` (`idcourse` ASC) VISIBLE,
   CONSTRAINT `iduser`
     FOREIGN KEY (`iduser`)
-    REFERENCES `sample2`.`user` (`iduser`)
+    REFERENCES `codemy`.`user` (`iduser`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `idcourse`
     FOREIGN KEY (`idcourse`)
-    REFERENCES `sample2`.`course` (`idcourse`)
+    REFERENCES `codemy`.`course` (`idcourse`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
